8372f18bd2531aec64ec3f36605017d400074975
Gregory Tahir <tahir.gregory.dev@gmail.com>
2025-02-15 13:57:21 +0100

    Mise en prod tes 1

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
+const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Configuration du transporteur Nodemailer
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -18,37 +18,39 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-// Route principale
 app.get("/", (req, res) => {
   res.send("Serveur Express fonctionne !");
 });
 
-// Route d'envoi d'email
 app.post("/send-email", async (req, res) => {
   const formData = req.body;
 
   try {
     if (!formData.firstName || !formData.lastName || !formData.email) {
-      return res.status(400).json({ success: false, message: 'Tous les champs sont requis' });
+      return res.status(400).json({ success: false, message: "Tous les champs sont requis" });
     }
 
     await transporter.sendMail({
       from: formData.email,
       to: process.env.EMAIL_USER,
       subject: `Nouveau message de ${formData.firstName} ${formData.lastName}`,
-      text: ` 
+      text: `
       Nom : ${formData.firstName}
-      Prenom : ${formData.lastName}
-      Email :  ${formData.email}
-
+      Prénom : ${formData.lastName}
+      Email : ${formData.email}
+    
       Message : 
-      ${formData.message}`
+      ${formData.message}
+      `,
     });
-
     res.status(200).json({ success: true, message: "Email envoyé !" });
   } catch (error) {
     res.status(500).json({ success: false, message: "Erreur d'envoi", error });
   }
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur le port ${PORT}`);
 });
 
 module.exports = app;
